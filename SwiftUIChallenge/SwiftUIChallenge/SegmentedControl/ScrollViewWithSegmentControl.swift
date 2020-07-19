5db4e5c55739c126de97a01ee9719bdd3aa98e29
Amit Majumdar <amit.majumdar89@gmail.com>
2020-07-19 12:00:26 +0530

    optional challenge

---
+++ SwiftUIChallenge/SwiftUIChallenge/SegmentedControl/ScrollViewWithSegmentControl.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewWithSegmentControl.swift
+//  SwiftUIChallenge
+//
+//  Created by Amit Majumdar on 19/07/20.
+//
+
+import SwiftUI
+
+struct ScrollViewWithSegmentControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViewWithSegmentControl_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewWithSegmentControl()
+    }
+}
